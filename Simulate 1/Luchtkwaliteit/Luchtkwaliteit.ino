9f1ff94215917ca3e750163365d77f2db595a125
KevinKDG <72076173+KevinKDG@users.noreply.github.com>
2020-12-22 10:31:58 +0100

    Add files via upload

---
+++ Simulate 1/Luchtkwaliteit/Luchtkwaliteit.ino
@@ -0,0 +1,21 @@
+// Voorbereiding code -----------------------------
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+// Loop -------------------------------------------
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+
+
+
+
+//Extra nota's -----------
+
+//Gemaakt door de groep Luchtkwaliteit
+//Graduaat IoT
+//Klas 1.1
